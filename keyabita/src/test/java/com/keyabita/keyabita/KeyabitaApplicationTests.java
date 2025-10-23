c6f2b174e300855741c3f55e6b707589b4bad76d
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-10-23 08:37:09 +0000

    setup

---
+++ keyabita/src/test/java/com/keyabita/keyabita/KeyabitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keyabita.keyabita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyabitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
