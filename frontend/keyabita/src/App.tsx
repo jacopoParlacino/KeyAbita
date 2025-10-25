7c55960d2a4bdb5e7dc5f93e52c7add5e09d9ee8
Olga Kutuzova <Olga.Kutuzova@edu-its.it>
2025-10-25 12:34:14 +0200

    Initial FE  Structure

---
+++ frontend/keyabita/src/App.tsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import "./styles/global.scss";
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Header></Header>
+      <main></main>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/keyabita/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer className="footer">
+            <p>KeyAbita &copy; 2025</p>
+        </footer>
+    );
+}
