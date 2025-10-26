4a0680b7062563cd9a1b5f7034ce5354a9151677
Olga Kutuzova <Olga.Kutuzova@edu-its.it>
2025-10-26 14:04:30 +0100

    Draft of a home page

--- frontend/keyabita/src/App.tsx
+++ frontend/keyabita/src/App.tsx
@@ -1,4 +1,6 @@
 // import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import "./styles/global.scss";
@@ -8,11 +10,15 @@ function App() {
   
 
   return (
-    <>
-      <Header></Header>
-      <main></main>
-      <Footer></Footer>
-    </>
+    <Router>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
