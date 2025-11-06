a1aa738ef9a6a9d8799c963550cce5f594e8ae70
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-11-06 20:17:38 +0100

    constructors model contratto

--- keyabita/src/main/java/com/keyabita/keyabita/model/Contratto.java
+++ keyabita/src/main/java/com/keyabita/keyabita/model/Contratto.java
@@ -26,5 +26,55 @@ public class Contratto {
     @JoinColumn(name = "stato_contratto", referencedColumnName = "id")
     private StatoContratto statoContratto;
 
+    public Contratto(){};
+
+    public Contratto(LocalDate inizioContratto, LocalDate fineContratto, Richiesta richiesta, StatoContratto statoContratto) {
+        this.inizioContratto = inizioContratto;
+        this.fineContratto = fineContratto;
+        this.richiesta = richiesta;
+        this.statoContratto = statoContratto;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public LocalDate getInizioContratto() {
+        return inizioContratto;
+    }
+
+    public void setInizioContratto(LocalDate inizioContratto) {
+        this.inizioContratto = inizioContratto;
+    }
+
+    public LocalDate getFineContratto() {
+        return fineContratto;
+    }
+
+    public void setFineContratto(LocalDate fineContratto) {
+        this.fineContratto = fineContratto;
+    }
+
+    public Richiesta getRichiesta() {
+        return richiesta;
+    }
+
+    public void setRichiesta(Richiesta richiesta) {
+        this.richiesta = richiesta;
+    }
+
+    public StatoContratto getStatoContratto() {
+        return statoContratto;
+    }
+
+    public void setStatoContratto(StatoContratto statoContratto) {
+        this.statoContratto = statoContratto;
+    }
+
+    
 
 }
