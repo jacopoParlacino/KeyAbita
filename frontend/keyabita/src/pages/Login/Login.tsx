cecbfb226125e479264762c7549f249afff11f74
ilda-05 <191975905+ilda-05@users.noreply.github.com>
2025-11-16 00:29:03 +0100

    form login api collegata al backend

--- frontend/keyabita/src/pages/Login/Login.tsx
+++ frontend/keyabita/src/pages/Login/Login.tsx
@@ -22,7 +22,7 @@ export default function Login() {
 
         try {
 
-        const res = await fetch("https://api", {
+        const res = await fetch("http://localhost:8080/api/auth/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({email, password}),
